jameslcowan/Ghost

commit 2cb972b2b99049a1a64d59fafade5efd9a3c7584

Updated ALL permissions migrations to use the utils
no-issue

core/server/data/migrations/versions/2.28/1-add-db-backup-content-permission.js
@@ -1,61 +1,9 @@
-const logging = require('../../../../../shared/logging');
-const models = require('../../../../models');
-const utils = require('../../../schema/fixtures/utils');
+const {
+    addPermission
+} = require('../../utils');
 
-const fixtureBackupContentPerm = utils.findModelFixtureEntry('Permission', {
-    object_type: 'db',
-    action_type: 'backupContent'
+module.exports = addPermission({
+    name: 'Backup database',
+    action: 'backupContect',
+    object: 'db'
 });
-
-module.exports = {
-    config: {
-        transaction: true
-    },
-
-    async up(options) {
-        try {
-            const existingBackupContentPerm = await models.Permission.findOne(
-                fixtureBackupContentPerm,
-                options
-            );
-
-            if (existingBackupContentPerm) {
-                return logging.warn('Issue adding db.backupContent (already exists)');
-            }
-
-            const result = await utils.addFixturesForModel({
-                name: 'Permission',
-                entries: [fixtureBackupContentPerm]
-            }, options);
-
-            const success = result.done === result.expected;
-
-            if (!success) {
-                return logging.warn('Issue adding db.backupContent permission (did not insert)');
-            }
-
-            return logging.info('Completed adding db.backupContent permission');
-        } catch (err) {
-            return logging.error('Errored when adding db.backupContent permission');
-        }
-    },
-
-    async down(options) {
-        try {
-            const existingBackupContentPerm = await models.Permission.findOne(
-                fixtureBackupContentPerm,
-                options
-            );
-
-            if (!existingBackupContentPerm) {
-                return logging.warn('Issue removing db.backupContent (already removed)');
-            }
-
-            await existingBackupContentPerm.destroy(options);
-
-            return logging.info('Completed removing db.backupContent permission');
-        } catch (err) {
-            return logging.error('Errored when removing db.backupContent permission');
-        }
-    }
-};

core/server/data/migrations/versions/1.13/2-theme-permissions.js
@@ -1,51 +1,58 @@
-const _ = require('lodash');
-const utils = require('../../../schema/fixtures/utils');
-const permissions = require('../../../../services/permissions');
-const logging = require('../../../../../shared/logging');
-const resource = 'theme';
-const _private = {};
+const {
+    addPermissionWithRoles,
+    combineTransactionalMigrations
+} = require('../../utils');
 
-_private.getPermissions = function getPermissions() {
-    return utils.findModelFixtures('Permission', {object_type: resource});
-};
-
-_private.getRelations = function getRelations() {
-    return utils.findPermissionRelationsForObject(resource);
-};
-
-_private.printResult = function printResult(result, message) {
-    if (result.done === result.expected) {
-        logging.info(message);
-    } else {
-        logging.warn('(' + result.done + '/' + result.expected + ') ' + message);
-    }
-};
-
-module.exports.config = {
-    transaction: true
-};
-
-module.exports.up = function addRedirectsPermissions(options) {
-    const modelToAdd = _private.getPermissions();
-    const relationToAdd = _private.getRelations();
-
-    const localOptions = _.merge({
-        context: {internal: true}
-    }, options);
-
-    return utils.addFixturesForModel(modelToAdd, localOptions)
-        .then(function (result) {
-            _private.printResult(result, 'Adding permissions fixtures for ' + resource + 's');
-            return utils.addFixturesForRelation(relationToAdd, localOptions);
-        })
-        .then(function (result) {
-            _private.printResult(result, 'Adding permissions_roles fixtures for ' + resource + 's');
-            return permissions.init(localOptions);
-        });
-};
-
-/**
- * @TODO:
- * The down function is quite tricky, because the fixture utility adds **all** missing fixtures for a target resource.
- * So if we are writing a down function, we are only allowed to remove the fixtures which were added between two Ghost versions.
- */
+module.exports = combineTransactionalMigrations(
+    addPermissionWithRoles({
+        name: 'Browse themes',
+        action: 'browse',
+        object: 'theme'
+    }, [
+        'Administrator',
+        'Admin Integration',
+        'Editor',
+        'Author',
+        'Contributor'
+    ]),
+    addPermissionWithRoles({
+        name: 'Edit themes',
+        action: 'edit',
+        object: 'theme'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Activate themes',
+        action: 'activate',
+        object: 'theme'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Upload themes',
+        action: 'add',
+        object: 'theme'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Download themes',
+        action: 'read',
+        object: 'theme'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Delete themes',
+        action: 'destroy',
+        object: 'theme'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ])
+);

core/server/data/migrations/versions/1.19/1-webhook-permissions.js
@@ -1,45 +1,31 @@
-const _ = require('lodash');
-const utils = require('../../../schema/fixtures/utils');
-const permissions = require('../../../../services/permissions');
-const logging = require('../../../../../shared/logging');
-const resource = 'webhook';
-const _private = {};
+const {
+    addPermissionWithRoles,
+    combineTransactionalMigrations
+} = require('../../utils');
 
-_private.getPermissions = function getPermissions() {
-    return utils.findModelFixtures('Permission', {object_type: resource});
-};
-
-_private.getRelations = function getRelations() {
-    return utils.findPermissionRelationsForObject(resource);
-};
-
-_private.printResult = function printResult(result, message) {
-    if (result.done === result.expected) {
-        logging.info(message);
-    } else {
-        logging.warn('(' + result.done + '/' + result.expected + ') ' + message);
-    }
-};
-
-module.exports.config = {
-    transaction: true
-};
-
-module.exports.up = function addRedirectsPermissions(options) {
-    const modelToAdd = _private.getPermissions();
-    const relationToAdd = _private.getRelations();
-
-    const localOptions = _.merge({
-        context: {internal: true}
-    }, options);
-
-    return utils.addFixturesForModel(modelToAdd, localOptions)
-        .then(function (result) {
-            _private.printResult(result, 'Adding permissions fixtures for ' + resource + 's');
-            return utils.addFixturesForRelation(relationToAdd, localOptions);
-        })
-        .then(function (result) {
-            _private.printResult(result, 'Adding permissions_roles fixtures for ' + resource + 's');
-            return permissions.init(localOptions);
-        });
-};
+module.exports = combineTransactionalMigrations(
+    addPermissionWithRoles({
+        name: 'Add webhooks',
+        action: 'add',
+        object: 'webhook'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Edit webhooks',
+        action: 'edit',
+        object: 'webhook'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Delete webhooks',
+        action: 'destroy',
+        object: 'webhook'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ])
+);

core/server/data/migrations/versions/1.9/1-add-permissions-redirect.js
@@ -1,50 +1,23 @@
-const _ = require('lodash');
-const utils = require('../../../schema/fixtures/utils');
-const permissions = require('../../../../services/permissions');
-const logging = require('../../../../../shared/logging');
-const resource = 'redirect';
-const _private = {};
+const {
+    addPermissionWithRoles,
+    combineTransactionalMigrations
+} = require('../../utils');
 
-_private.getPermissions = function getPermissions() {
-    return utils.findModelFixtures('Permission', {object_type: resource});
-};
-
-_private.getRelations = function getRelations() {
-    return utils.findPermissionRelationsForObject(resource);
-};
-
-_private.printResult = function printResult(result, message) {
-    if (result.done === result.expected) {
-        logging.info(message);
-    } else {
-        logging.warn('(' + result.done + '/' + result.expected + ') ' + message);
-    }
-};
-
-module.exports.config = {
-    transaction: true
-};
-
-module.exports.up = function addRedirectsPermissions(options) {
-    const modelToAdd = _private.getPermissions();
-    const relationToAdd = _private.getRelations();
-
-    const localOptions = _.merge({
-        context: {internal: true}
-    }, options);
-
-    return utils.addFixturesForModel(modelToAdd, localOptions).then(function (result) {
-        _private.printResult(result, 'Adding permissions fixtures for ' + resource + 's');
-        return utils.addFixturesForRelation(relationToAdd, localOptions);
-    }).then(function (result) {
-        _private.printResult(result, 'Adding permissions_roles fixtures for ' + resource + 's');
-    }).then(function () {
-        return permissions.init(localOptions);
-    });
-};
-
-/**
- * @TODO:
- * The down function is quite tricky, because the fixture utility adds **all** missing fixtures for a target resource.
- * So if we are writing a down function, we are only allowed to remove the fixtures which were added between two Ghost versions.
- */
+module.exports = combineTransactionalMigrations(
+    addPermissionWithRoles({
+        name: 'Download redirects',
+        action: 'download',
+        object: 'redirect'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Upload redirects',
+        action: 'download',
+        object: 'redirect'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ])
+);

core/server/data/migrations/versions/2.14/2-add-actions-permissions.js
@@ -1,47 +1,12 @@
-const _ = require('lodash');
-const utils = require('../../../schema/fixtures/utils');
-const permissions = require('../../../../services/permissions');
-const logging = require('../../../../../shared/logging');
-const resource = 'action';
-const _private = {};
+const {
+    addPermissionWithRoles
+} = require('../../utils');
 
-_private.getPermissions = function getPermissions() {
-    return utils.findModelFixtures('Permission', {object_type: resource});
-};
-
-_private.getRelations = function getRelations() {
-    return utils.findPermissionRelationsForObject(resource);
-};
-
-_private.printResult = function printResult(result, message) {
-    if (result.done === result.expected) {
-        logging.info(message);
-    } else {
-        logging.warn('(' + result.done + '/' + result.expected + ') ' + message);
-    }
-};
-
-module.exports.config = {
-    transaction: true
-};
-
-module.exports.up = function addRedirectsPermissions(options) {
-    const modelToAdd = _private.getPermissions();
-    const relationToAdd = _private.getRelations();
-    const localOptions = _.merge({
-        context: {
-            internal: true,
-            migrating: true
-        }
-    }, options);
-
-    return utils.addFixturesForModel(modelToAdd, localOptions)
-        .then(function (result) {
-            _private.printResult(result, 'Adding permissions fixtures for ' + resource + 's');
-            return utils.addFixturesForRelation(relationToAdd, localOptions);
-        })
-        .then(function (result) {
-            _private.printResult(result, 'Adding permissions_roles fixtures for ' + resource + 's');
-            return permissions.init(localOptions);
-        });
-};
+module.exports = addPermissionWithRoles({
+    name: 'Browse Actions',
+    action: 'browse',
+    object: 'action'
+}, [
+    'Administrator',
+    'Admin Integration'
+]);

core/server/data/migrations/versions/2.2/4-insert-integration-and-api-key-permissions.js
@@ -1,58 +1,77 @@
-const _ = require('lodash');
-const utils = require('../../../schema/fixtures/utils');
-const permissions = require('../../../../services/permissions');
-const logging = require('../../../../../shared/logging');
+const {
+    addPermissionWithRoles,
+    combineTransactionalMigrations
+} = require('../../utils');
 
-const resources = ['integration', 'api_key'];
-const _private = {};
-
-_private.getPermissions = function getPermissions(resource) {
-    return utils.findModelFixtures('Permission', {object_type: resource});
-};
-
-_private.getRelations = function getRelations(resource) {
-    return utils.findPermissionRelationsForObject(resource);
-};
-
-_private.printResult = function printResult(result, message) {
-    if (result.done === result.expected) {
-        logging.info(message);
-    } else {
-        logging.warn(`(${result.done}/${result.expected}) ${message}`);
-    }
-};
-
-module.exports.config = {
-    transaction: true
-};
-
-module.exports.up = (options) => {
-    const localOptions = _.merge({
-        context: {internal: true}
-    }, options);
-
-    return Promise.map(resources, (resource) => {
-        const modelToAdd = _private.getPermissions(resource);
-        const relationToAdd = _private.getRelations(resource);
-
-        return utils.addFixturesForModel(modelToAdd, localOptions)
-            .then(result => _private.printResult(result, `Adding permissions fixtures for ${resource}s`))
-            .then(() => utils.addFixturesForRelation(relationToAdd, localOptions))
-            .then(result => _private.printResult(result, `Adding permissions_roles fixtures for ${resource}s`))
-            .then(() => permissions.init(localOptions));
-    });
-};
-
-module.exports.down = (options) => {
-    const localOptions = _.merge({
-        context: {internal: true}
-    }, options);
-
-    return Promise.map(resources, (resource) => {
-        const modelToRemove = _private.getPermissions(resource);
-
-        // permission model automatically cleans up permissions_roles on .destroy()
-        return utils.removeFixturesForModel(modelToRemove, localOptions)
-            .then(result => _private.printResult(result, `Removing permissions fixtures for ${resource}s`));
-    });
-};
+module.exports = combineTransactionalMigrations(
+    addPermissionWithRoles({
+        name: 'Browse integrations',
+        action: 'browse',
+        object: 'integration'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Read integrations',
+        action: 'read',
+        object: 'integration'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Edit integrations',
+        action: 'edit',
+        object: 'integration'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Add integrations',
+        action: 'add',
+        object: 'integration'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Delete integrations',
+        action: 'destroy',
+        object: 'integration'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Browse API keys',
+        action: 'browse',
+        object: 'api_key'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Read API keys',
+        action: 'read',
+        object: 'api_key'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Edit API keys',
+        action: 'edit',
+        object: 'api_key'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Add API keys',
+        action: 'add',
+        object: 'api_key'
+    }, [
+        'Administrator'
+    ]),
+    addPermissionWithRoles({
+        name: 'Delete API keys',
+        action: 'destroy',
+        object: 'api_key'
+    }, [
+        'Administrator'
+    ])
+);

core/server/data/migrations/versions/2.21/1-update-editor-permissions.js
@@ -1,51 +1,28 @@
-const _ = require('lodash');
-const utils = require('../../../schema/fixtures/utils');
-const permissions = require('../../../../services/permissions');
-const logging = require('../../../../../shared/logging');
+const {
+    combineTransactionalMigrations,
+    addPermissionWithRoles
+} = require('../../utils');
 
-const resources = ['notification'];
-
-const _private = {};
-
-_private.getRelations = function getRelations(resource, role) {
-    return utils.findPermissionRelationsForObject(resource, role);
-};
-
-_private.printResult = function printResult(result, message) {
-    if (result.done === result.expected) {
-        logging.info(message);
-    } else {
-        logging.warn(`(${result.done}/${result.expected}) ${message}`);
-    }
-};
-
-module.exports.config = {
-    transaction: true
-};
-
-module.exports.up = (options) => {
-    const localOptions = _.merge({
-        context: {internal: true}
-    }, options);
-
-    return Promise.map(resources, (resource) => {
-        const relations = _private.getRelations(resource, 'Editor');
-
-        return Promise.resolve()
-            .then(() => utils.addFixturesForRelation(relations, localOptions))
-            .then(result => _private.printResult(result, `Adding permissions_roles fixtures for ${resource}s`))
-            .then(() => permissions.init(localOptions));
-    });
-};
-
-module.exports.down = (options) => {
-    const localOptions = _.merge({
-        context: {internal: true}
-    }, options);
-
-    return Promise.map(resources, (resource) => {
-        const relations = _private.getRelations(resource, 'Editor');
-
-        return utils.removeFixturesForRelation(relations, localOptions);
-    });
-};
+module.exports = combineTransactionalMigrations(
+    addPermissionWithRoles({
+        name: 'Browse notifications',
+        action: 'browse',
+        object: 'notification'
+    }, [
+        'Editor'
+    ]),
+    addPermissionWithRoles({
+        name: 'Add notifications',
+        action: 'add',
+        object: 'notification'
+    }, [
+        'Editor'
+    ]),
+    addPermissionWithRoles({
+        name: 'Delete notifications',
+        action: 'destroy',
+        object: 'notification'
+    }, [
+        'Editor'
+    ])
+);

core/server/data/migrations/versions/2.22/1-add-member-permissions-to-roles.js
@@ -1,49 +1,47 @@
-const _ = require('lodash');
-const utils = require('../../../schema/fixtures/utils');
-const permissions = require('../../../../services/permissions');
-const logging = require('../../../../../shared/logging');
-const resource = 'member';
-const _private = {};
+const {
+    addPermissionWithRoles,
+    combineTransactionalMigrations
+} = require('../../utils');
 
-_private.getPermissions = function getPermissions() {
-    return utils.findModelFixtures('Permission', {object_type: resource});
-};
-
-_private.getRelations = function getRelations() {
-    return utils.findPermissionRelationsForObject(resource);
-};
-
-_private.printResult = function printResult(result, message) {
-    if (result.done === result.expected) {
-        logging.info(message);
-    } else {
-        logging.warn('(' + result.done + '/' + result.expected + ') ' + message);
-    }
-};
-
-module.exports.up = function addMembersPermissions(options) {
-    const modelToAdd = _private.getPermissions();
-    const relationToAdd = _private.getRelations();
-    const localOptions = _.merge({
-        context: {
-            internal: true,
-            migrating: true
-        }
-    }, options);
-
-    return utils.addFixturesForModel(modelToAdd, localOptions)
-        .then(function (result) {
-            _private.printResult(
-                result,
-                `Adding permissions fixtures for ${resource}s`
-            );
-            return utils.addFixturesForRelation(relationToAdd, localOptions);
-        })
-        .then(function (result) {
-            _private.printResult(
-                result,
-                `Adding permissions_roles fixtures for ${resource}s`
-            );
-            return permissions.init(localOptions);
-        });
-};
+module.exports = combineTransactionalMigrations(
+    addPermissionWithRoles({
+        name: 'Browse Members',
+        action: 'browse',
+        object: 'member'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Read Members',
+        action: 'read',
+        object: 'member'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Edit Members',
+        action: 'edit',
+        object: 'member'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Add Members',
+        action: 'add',
+        object: 'member'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ]),
+    addPermissionWithRoles({
+        name: 'Delete Members',
+        action: 'destroy',
+        object: 'member'
+    }, [
+        'Administrator',
+        'Admin Integration'
+    ])
+);

commit a979c01677e0a0cfb10f3924ca604eb784d03472

Fixed typo in migration to add backupContent permission (#12776)
refs https://github.com/TryGhost/Team/issues/553

A typo in the permission migration was introduced in
https://github.com/TryGhost/Ghost/commit/2cb972b2

A migration to fix broken values will be present in Ghost 4.1

core/server/data/migrations/versions/2.28/1-add-db-backup-content-permission.js
@@ -4,6 +4,6 @@ const {
 
 module.exports = addPermission({
     name: 'Backup database',
-    action: 'backupContect',
+    action: 'backupContent',
     object: 'db'
 });